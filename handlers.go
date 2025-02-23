2f9982f80e35756d8b5163b7e11d94e4acdada20
Zenoviia <zenoviatkacenko@gmail.com>
2025-02-23 22:31:49 +0200

    Add handlers logic

--- handlers.go
+++ handlers.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	"encoding/json"
+	"log"
 	"net/http"
+	"time"
+	
 )
 
 func TimeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	response := TimeResponse{Time: time.Now().Format(time.RFC3339)}
 }
